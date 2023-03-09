f8af96727990b8acd49d46bc87beada3f895276a
artemkuc44 <artjoms.kucajevs@ucdconnect.ie>
2023-03-09 17:32:54 +0000

    09/03/2023
    made a small start to the project

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    int x = 4, y = 5;
+int main(void) {
+    int numTasks;
+    printf("Enter how many tasks you would like to enter");
+    scanf("%d",&numTasks);
 
-    printf("%d", x * y);
-    
+    printf("          |January   |February  |March     |April     |May       |June      |July      |August    |September |October   |November  |December  |\n");
+
+
+    for(int i = 0;i<numTasks;i++){
+      for(int k = 0;k<13;k++){
+          printf("----------|");
+      }
+      printf("\n");
+      for(int j = 0;j<13;j++){
+          printf("xxxxxxxxxx|");
+      }
+      printf("\n");
+    }
+    for(int i = 0;i<13;i++){
+      printf("----------|");
+    }
     return 0;
 }
